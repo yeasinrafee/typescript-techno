d8dbf38e1c7ad5dd25955a7eea09613a03322010
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-06 15:26:56 +0600

    Asynchronous typescript

---
+++ module2/2.8.ts
@@ -0,0 +1,22 @@
+{
+  // Promise
+
+  type Todo = {
+    id: number;
+    userId: number;
+    title: string;
+    completed: boolean;
+  };
+
+  const getTodo = async (): Promise<Todo> => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    return data;
+    // console.log(data);
+  };
+  getTodo();
+
+  //
+}
