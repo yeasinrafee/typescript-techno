5a52c99eabc8d09b1d99c2a7d476e9e8ad555fe2
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-06 15:37:02 +0600

    conditional types

--- module2/2.9.ts
+++ module2/2.9.ts
@@ -1,4 +1,20 @@
 {
   // Conditional type
+  type a1 = number;
+  type b1 = string;
+
+  type x = a1 extends null ? true : false; // conditional type
+  type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+  type assets = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+
+  type CheckVehicle<T> = T extends keyof assets ? true : false;
+
+  type HasBike = CheckVehicle<"bike">;
+
   //
 }
