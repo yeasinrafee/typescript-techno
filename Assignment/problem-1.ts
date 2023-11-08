d1f4ed33d0bf35029435646daa56bbe766464341
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-09 01:28:27 +0600

    problem-1

---
+++ Assignment/problem-1.ts
@@ -0,0 +1,12 @@
+// Design a TypeScript function that takes a parameter of a union type (e.g., string | number). If it's a string, return its length. If it's a number, return the square of that number.
+
+type Data = string | number;
+const lengthOrSquare = (data: Data): Data => {
+  if (typeof data === "string") {
+    return data.length;
+  } else {
+    return data * data;
+  }
+};
+console.log(lengthOrSquare("12345"));
+console.log(lengthOrSquare(5));
