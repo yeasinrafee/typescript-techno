971ae89834b6c5b58bc087709b26858130642534
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-09 15:20:47 +0600

    Problem-8

--- Assignment/problem-7.ts
+++ Assignment/problem-7.ts
@@ -1,5 +1,6 @@
 // Create a TypeScript function called findFirstOccurrence that accepts an array and a value to search for. Use generics to make the function work with arrays of any data type. Inside the function, find and return the index of the first occurrence of the value in the array. If the value is not found in the array, return -1 to indicate that. Create sample arrays of different data types (e.g., numbers, strings) and call the findFirstOccurrence function for each of them to display the results.
 
+// Problem: 7
 const findFirstOccurrence = <T>(arr: T[], element: T): number => {
   const index = arr.indexOf(element);
   if (index >= 0) {
--- Assignment/problem-8.ts
+++ Assignment/problem-8.ts
@@ -1,1 +1,22 @@
 // Create a TypeScript program that simulates a simple shopping cart. Define an interface Product with properties like name, price, and quantity. Implement a function that calculates the total cost of the items in the shopping cart. The function should take an array of Product objects as input and return the total cost.
+
+interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+const totalCost = (carts: Product[]): number => {
+  let cost: number = 0;
+  carts.forEach((cart) => (cost += cart.price * cart.quantity));
+  return cost;
+};
+
+const shoppingCart: Product[] = [
+  { name: "Item 1", price: 20, quantity: 2 },
+  { name: "Item 2", price: 20, quantity: 3 },
+  { name: "Item 3", price: 0, quantity: 1 },
+];
+
+const talCost = totalCost(shoppingCart);
+console.log("Total Cost:", talCost);
